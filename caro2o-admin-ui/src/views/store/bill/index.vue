6259911f818f427181eafb3c9aec2591912e102f
dxy <2513524891@qq.com>
2022-11-27 20:16:30 +0800

    出入仓库表bug

---
+++ caro2o-admin-ui/src/views/store/bill/index.vue
@@ -0,0 +1,378 @@
+<template>
+  <div class="app-container">
+    <el-form :model="queryParams" ref="queryForm" size="small" :inline="true" v-show="showSearch" label-width="68px">
+      <el-form-item label="出入库时间">
+        <el-date-picker
+          v-model="daterangeBusiDate"
+          style="width: 240px"
+          value-format="yyyy-MM-dd"
+          type="daterange"
+          range-separator="-"
+          start-placeholder="开始日期"
+          end-placeholder="结束日期"
+        ></el-date-picker>
+      </el-form-item>
+      <el-form-item label="类型" prop="type">
+        <el-select v-model="queryParams.type" placeholder="请选择类型" clearable>
+          <el-option
+            v-for="dict in dict.type.sb_type"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+
+      <el-form-item label="状态" prop="status">
+        <el-select v-model="queryParams.status" placeholder="请选择状态" clearable>
+          <el-option
+            v-for="dict in dict.type.sb_status"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item label="仓库" prop="status">
+        <el-select v-model="queryParams.status" placeholder="请选择仓库" clearable>
+          <el-option
+            v-for="dict in dict.type.sb_status"
+            :key="dict.value"
+            :label="dict.label"
+            :value="dict.value"
+          />
+        </el-select>
+      </el-form-item>
+      <el-form-item>
+        <el-button type="primary" icon="el-icon-search" size="mini" @click="handleQuery">搜索</el-button>
+        <el-button icon="el-icon-refresh" size="mini" @click="resetQuery">重置</el-button>
+      </el-form-item>
+    </el-form>
+
+    <el-row :gutter="10" class="mb8">
+      <el-col :span="1.5">
+        <el-button
+          type="primary"
+          plain
+          icon="el-icon-plus"
+          size="mini"
+          @click="handleAdd"
+          v-hasPermi="['store:bill:add']"
+        >入库</el-button>
+      </el-col>
+      <el-col :span="1.5">
+        <el-button
+          type="success"
+          plain
+          icon="el-icon-edit"
+          size="mini"
+          :disabled="single"
+          @click="handleUpdate"
+          v-hasPermi="['store:bill:edit']"
+        >出库</el-button>
+      </el-col>
+      <right-toolbar :showSearch.sync="showSearch" @queryTable="getList"></right-toolbar>
+    </el-row>
+
+    <el-table v-loading="loading" :data="billList" @selection-change="handleSelectionChange">
+      <el-table-column type="selection" width="55" align="center" />
+      <el-table-column label="序号" align="center" prop="id" />
+      <el-table-column label="类型" align="center" prop="type">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.sb_type" :value="scope.row.type"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="仓库" align="center" prop="storeId" />
+      <el-table-column label="总数量" align="center" prop="totalNum" />
+      <el-table-column label="总金额" align="center" prop="totalMoney" />
+      <el-table-column label="录入人" align="center" prop="operatorId" />
+      <el-table-column label="出入库时间" align="center" prop="busiDate" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.busiDate, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="录入时间" align="center" prop="operateDate" width="180">
+        <template slot-scope="scope">
+          <span>{{ parseTime(scope.row.operateDate, '{y}-{m}-{d}') }}</span>
+        </template>
+      </el-table-column>
+      <el-table-column label="状态" align="center" prop="status">
+        <template slot-scope="scope">
+          <dict-tag :options="dict.type.sb_status" :value="scope.row.status"/>
+        </template>
+      </el-table-column>
+      <el-table-column label="操作" align="center" class-name="small-padding fixed-width">
+        <template slot-scope="scope">
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-delete"
+            @click="handleDelete(scope.row)"
+            v-hasPermi="['store:bill:edit']"
+            v-if="scope.row.status==0"
+          >作废</el-button>
+          <el-button
+            size="mini"
+            type="text"
+            icon="el-icon-edit"
+            @click="handleQuery(scope.row)"
+            v-hasPermi="['store:bill:remove']"
+          >查看</el-button>
+        </template>
+      </el-table-column>
+    </el-table>
+
+    <pagination
+      v-show="total>0"
+      :total="total"
+      :page.sync="queryParams.pageNum"
+      :limit.sync="queryParams.pageSize"
+      @pagination="getList"
+    />
+
+    <!-- 添加或修改出入库单据对话框 -->
+    <el-dialog :title="title" :visible.sync="open" width="1000px" append-to-body>
+
+
+      <el-form ref="form" :model="form" :rules="rules" label-width="80px">
+        <el-form-item label="类型" prop="type">
+          <el-select v-model="form.type" placeholder="请选择类型">
+            <el-option
+              v-for="dict in dict.type.sb_type"
+              :key="dict.value"
+              :label="dict.label"
+:value="parseInt(dict.value)"
+            ></el-option>
+          </el-select>
+        </el-form-item>
+        <el-form-item label="出入库时间" prop="busiDate">
+          <el-date-picker clearable
+            v-model="form.busiDate"
+            type="date"
+            value-format="yyyy-MM-dd"
+            placeholder="请选择出入库时间">
+          </el-date-picker>
+        </el-form-item>
+        <el-form-item label="备注" prop="remark">
+          <el-input v-model="form.remark" type="textarea" :rows="5"  style="width:40%" placeholder="请输入内容" />
+        </el-form-item>
+        <el-row :gutter="10" class="mb8">
+          <el-col :span="1.5">
+            <el-button
+              type="primary"
+              plain
+              icon="el-icon-plus"
+              size="mini"
+              @click="handleAdd"
+              v-hasPermi="['store:bill:add']"
+            >入库</el-button>
+          </el-col>
+        </el-row>
+        <el-table
+          :data="itemData"
+          border
+          style="width: 100%">
+          <el-table-column
+            fixed
+            prop="date"
+            label="序号"
+            width="150">
+          </el-table-column>
+          <el-table-column
+            prop="name"
+            label="物品"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="province"
+            label="价格"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="city"
+            label="数量"
+            width="120">
+          </el-table-column>
+          <el-table-column
+            prop="address"
+            label="小计"
+            width="300">
+          </el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="100">
+            <template slot-scope="scope">
+              <el-button
+                size="mini"
+                type="text"
+                icon="el-icon-edit"
+                @click="itemDelete(scope.row)"
+              >删除</el-button>
+            </template>
+          </el-table-column>
+        </el-table>
+      </el-form>
+      <div slot="footer" class="dialog-footer">
+        <el-button type="primary" @click="submitForm">确 定</el-button>
+        <el-button @click="cancel">取 消</el-button>
+      </div>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+import { listBill, getBill, delBill, addBill, updateBill } from "@/api/store/bill";
+import { listBillItem, getBillItem, delBillItem, addBillItem, updateBillItem } from "@/api/store/billItem";
+
+export default {
+  name: "Bill",
+  dicts: ['sb_type', 'sb_status'],
+  data() {
+    return {
+      itemData:[],
+      // 遮罩层
+      loading: true,
+      // 选中数组
+      ids: [],
+      // 非单个禁用
+      single: true,
+      // 非多个禁用
+      multiple: true,
+      // 显示搜索条件
+      showSearch: true,
+      // 总条数
+      total: 0,
+      // 出入库单据表格数据
+      billList: [],
+      // 弹出层标题
+      title: "",
+      // 是否显示弹出层
+      open: false,
+      // 备注时间范围
+      daterangeBusiDate: [],
+      // 查询参数
+      queryParams: {
+        pageNum: 1,
+        pageSize: 10,
+        type: null,
+        storeId: null,
+        busiDate: null,
+        status: null,
+      },
+      // 表单参数
+      form: {},
+      // 表单校验
+      rules: {
+      }
+    };
+  },
+  created() {
+    this.getList();
+  },
+  methods: {
+    /** 查询出入库单据列表 */
+    getList() {
+      this.loading = true;
+      this.queryParams.params = {};
+      if (null != this.daterangeBusiDate && '' != this.daterangeBusiDate) {
+        this.queryParams.params["beginBusiDate"] = this.daterangeBusiDate[0];
+        this.queryParams.params["endBusiDate"] = this.daterangeBusiDate[1];
+      }
+      listBill(this.queryParams).then(response => {
+        this.billList = response.rows;
+        this.total = response.total;
+        this.loading = false;
+      });
+    },
+    // 取消按钮
+    cancel() {
+      this.open = false;
+      this.reset();
+    },
+    // 表单重置
+    reset() {
+      this.form = {
+        id: null,
+        type: null,
+        storeId: null,
+        busiDate: null,
+        status: null,
+        operateDate: null,
+        operatorId: null,
+        remark: null
+      };
+      this.resetForm("form");
+    },
+    /** 搜索按钮操作 */
+    handleQuery() {
+      this.queryParams.pageNum = 1;
+      this.getList();
+    },
+    /** 重置按钮操作 */
+    resetQuery() {
+      this.daterangeBusiDate = [];
+      this.resetForm("queryForm");
+      this.handleQuery();
+    },
+    // 多选框选中数据
+    handleSelectionChange(selection) {
+      this.ids = selection.map(item => item.id)
+      this.single = selection.length!==1
+      this.multiple = !selection.length
+    },
+    /** 新增按钮操作 */
+    handleAdd() {
+      this.reset();
+      this.open = true;
+      this.title = "添加出入库单据";
+    },
+    /** 修改按钮操作 */
+    handleUpdate(row) {
+      this.reset();
+      const id = row.id || this.ids
+      getBill(id).then(response => {
+        this.form = response.data;
+        this.open = true;
+        this.title = "修改出入库单据";
+      });
+    },
+    /** 提交按钮 */
+    submitForm() {
+      this.$refs["form"].validate(valid => {
+        if (valid) {
+          if (this.form.id != null) {
+            updateBill(this.form).then(response => {
+              this.$modal.msgSuccess("修改成功");
+              this.open = false;
+              this.getList();
+            });
+          } else {
+            addBill(this.form).then(response => {
+              this.$modal.msgSuccess("新增成功");
+              this.open = false;
+              this.getList();
+            });
+          }
+        }
+      });
+    },
+    /** 删除按钮操作 */
+    handleDelete(row) {
+      const ids = row.id || this.ids;
+      this.$modal.confirm('是否确认删除出入库单据编号为"' + ids + '"的数据项？').then(function() {
+        return delBill(ids);
+      }).then(() => {
+        this.getList();
+        this.$modal.msgSuccess("删除成功");
+      }).catch(() => {});
+    },
+    /** 导出按钮操作 */
+    handleExport() {
+      this.download('store/bill/export', {
+        ...this.queryParams
+      }, `bill_${new Date().getTime()}.xlsx`)
+    }
+  }
+};
+</script>
