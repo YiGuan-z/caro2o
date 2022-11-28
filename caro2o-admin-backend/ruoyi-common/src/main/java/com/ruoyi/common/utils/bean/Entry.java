d251170a7ce9cccad27581761f47d57a1c058325
Casey Cheng <61931277+YiGuan-z@users.noreply.github.com>
2022-11-28 09:04:36 +0800

    后端方式树结构生成

--- caro2o-admin-backend/ruoyi-common/src/main/java/com/ruoyi/common/utils/bean/Entry.java
+++ caro2o-admin-backend/ruoyi-common/src/main/java/com/ruoyi/common/utils/bean/Entry.java
@@ -3,6 +3,8 @@ package com.ruoyi.common.utils.bean;
 import java.io.Serializable;
 import java.lang.reflect.Field;
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.stream.Collectors;
 
 /**
--- caro2o-admin-backend/ruoyi-workflow/src/main/java/com/ruoyi/workflow/service/impl/GoodsCategoryServiceImpl.java
+++ caro2o-admin-backend/ruoyi-workflow/src/main/java/com/ruoyi/workflow/service/impl/GoodsCategoryServiceImpl.java
@@ -1,6 +1,8 @@
 package com.ruoyi.workflow.service.impl;
 
-import java.util.List;
+import java.util.*;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import com.ruoyi.workflow.mapper.GoodsCategoryMapper;
@@ -26,4 +28,30 @@ public class GoodsCategoryServiceImpl extends ServiceImpl<GoodsCategoryMapper, G
     public List<GoodsCategory> selectGoodsCategoryList(GoodsCategory goodsCategory) {
         return getBaseMapper().selectGoodsCategoryList(goodsCategory);
     }
+	
+	@Override
+	public List<GoodsCategory> getTreeData() {
+		List<GoodsCategory> ret=new ArrayList<>(10);
+		final List<GoodsCategory> categories = baseMapper.selectList(null);
+		final Map<String,GoodsCategory> map = categories.stream()
+				.collect(Collectors.toMap(GoodsCategory::getBusiPath, goods -> goods));
+		map.forEach((key,node)->{
+			final int nodeLenght = key.split(":").length;
+			if (nodeLenght==1){
+				ret.add(node);
+			}else {
+				final String parentKey = key.substring(0, key.length() - 2);
+				final GoodsCategory goodsCategory = map.get(parentKey);
+				goodsCategory.getChildren().add(node);
+			}
+		});
+		return ret;
+	}
+	//删除分类时连同子分类一起删除
+	//删除分类时如当前分类连同子分类下的商品还有库存则不可以删除
+	@Override
+	public boolean removeBatchByIds(Collection<?> list) {
+		
+		return super.removeBatchByIds(list);
+	}
 }
